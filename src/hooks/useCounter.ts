32e95e2dc1b1bd68199db94b591522df3aa3c7e8
Anusha Syeda <anoshasyeda1999@gmail.com>
2026-04-30 13:06:21 +0500

    Add App component, counter hook, and ESLint config

---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const useCounter = () => {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  return {
+    count,
+    handleIncrement,
+  };
+};
